e5d34d66bcc6ffacbbe71ca635b8381848b42b21
reza <rezanaeimabadi7@gmail.com>
2022-12-11 13:37:33 -0700

    added path to router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from "./projects/projects.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'projects-component', component: ProjectsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
