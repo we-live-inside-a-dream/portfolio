10c8f769597a060358a3ad211f0493840ac37be2
reza <rezanaeimabadi7@gmail.com>
2022-12-10 19:07:47 -0700

    added greeting section to homepage class

--- src/app/content/homePage.ts
+++ src/app/content/homePage.ts
@@ -3,5 +3,6 @@
 export const homePage = {
   name: "REZA NAEIM-ABADI",
   title: "Software Engineer",
-  description: 
+  greeting: '“Howdy ho!”',
+  introduction: 'I’m a Software-Developer, Martial arts warrior and Fueled by mostly plant and a bit of meat here and there. Driven by passion and Coffee.'
 }
