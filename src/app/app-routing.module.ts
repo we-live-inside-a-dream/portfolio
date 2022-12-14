5042b3a7d61765b8cc7299fccf3cd8b906434683
reza <rezanaeimabadi7@gmail.com>
2022-12-14 13:46:09 -0700

    created navbar routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProjectsComponent } from "./projects/projects.component";
+import { NavbarComponent } from "./navbar/navbar.component";
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'projects-component', component: ProjectsComponent},
+  {path: 'navbar-component', component: NavbarComponent}
 ];
 
 @NgModule({
