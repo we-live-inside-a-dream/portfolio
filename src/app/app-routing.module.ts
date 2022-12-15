7dcd7610bd8f839a859d768e89c7894a8ff7938c
reza <rezanaeimabadi7@gmail.com>
2022-12-14 21:16:13 -0700

    removed navigation bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProjectsComponent } from "./projects/projects.component";
-import { NavbarComponent } from "./navbar/navbar.component";
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'projects-component', component: ProjectsComponent},
-  {path: 'navbar-component', component: NavbarComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ProjectsComponent } from './projects/projects.component';
-import { NavbarComponent} from "./navbar/navbar.component";
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
@@ -13,7 +11,6 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     AppComponent,
     HomeComponent,
     ProjectsComponent,
-    NavbarComponent
   ],
   imports: [
     BrowserModule,
