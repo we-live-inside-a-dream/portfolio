6137a25dfb29fc481afe40d7441b255c7b9eeb38
reza <rezanaeimabadi7@gmail.com>
2022-12-25 13:53:59 -0700

    styled slider

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import SwiperCore, {Navigation, Pagination, Scrollbar, A11y} from 'swiper';
+
+// install Swiper modules
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  styleUrls: ['./projects.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ProjectsComponent implements OnInit {
 
-  constructor() { }
-
   ngOnInit(): void {
   }
+
+
 
 }
